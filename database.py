fd3c6827c6b4d79266ed8cbcc425b1dce60f8584
Stefan <stefan_micevski@ymail.com>
2025-08-27 19:14:05 +0200

    Added fully functional recommendations and notification

--- database.py
+++ database.py
@@ -1,4 +1,49 @@
 import sqlite3
 
-conn = sqlite3.connect("weather_data.db")
-cursor = conn.cursor()
+
+def create_table():
+    connection = sqlite3.connect("weather_data.db")
+    cursor = connection.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS WEATHER (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        temperature REAL,
+        feels REAL,
+        rain INT DEFAULT 0,
+        humidity INT,
+        condition TEXT,
+        date TEXT
+    )
+    """)
+    connection.commit()
+    connection.close()
+
+def delete_weather_data():
+    connection = sqlite3.connect("weather_data.db")
+    cursor = connection.cursor()
+    cursor.execute("DELETE FROM WEATHER")
+    cursor.execute("DELETE FROM sqlite_sequence WHERE name='WEATHER'")
+    connection.commit()
+    connection.close()
+
+def insert_weather_data(temperature, feelslike, rain, humidity, condition, datetime):
+    connection = sqlite3.connect("weather_data.db")
+    cursor = connection.cursor()
+    cursor.execute("""
+    INSERT INTO WEATHER (temperature, feels, rain, humidity, condition, date)
+    VALUES (?, ?, ?, ?, ?, ?)""", (temperature, feelslike, rain, humidity, condition, datetime))
+    connection.commit()
+    connection.close()
+
+def show_weather_data():
+    connection = sqlite3.connect("weather_data.db")
+    cursor = connection.cursor()
+    cursor.execute("""
+    SELECT * FROM WEATHER 
+        ORDER BY date ASC 
+        LIMIT 1""")
+    rows = cursor.fetchall()
+    connection.close()
+    return rows[0]
+
+
--- weather.py
+++ weather.py
@@ -1,80 +1,151 @@
+from socket import timeout
 import requests
 import json
-import schedule
+import database
+from plyer import notification
 import time
 
-city = "Skopje"
+city = "YOUR_CITY_HERE"
 unit = "metric"
-api_key = "your_api_key_here"
+api_key = "YOUR_API_KEY_HERE"
 url = f"https://api.openweathermap.org/data/2.5/forecast?q={city}&units={unit}&appid={api_key}"
 
-def fetch_data(url):
+def fetch_data():
     response = requests.get(url)
     data = response.json()
-    return data
-
-def get_temp(data):
-    forecast_list = data["list"]
-    temp = forecast_list[0]["main"]["temp"]
-    feelslike = forecast_list[0]["main"]["feels_like"]
-    avg_temp = (float(temp)+float(feelslike))/2
-    if avg_temp < 0:
-        print("❄️ It’s freezing outside! Wear a heavy winter coat, gloves, and a hat 🧥🧤🧣.")
-    elif avg_temp >= 0 and avg_temp < 10:
-        print("🧥 Quite chilly. A warm jacket and layers are recommended 🧣🧦.")
-    elif avg_temp >= 10 and avg_temp < 20:
-        print("🌤️ Mild weather. A light jacket or sweater should be enough 🧥.")
-    elif avg_temp >= 20 and avg_temp < 30:
-        print("😊 Comfortable and warm. Short sleeves or a T-shirt are perfect 👕.")
-    elif avg_temp >= 30 and avg_temp < 40:
-        print("🔥 It’s hot outside! Wear breathable clothes, stay hydrated 💧🧢.")
-    else:
-        print("☀️ Extreme heat alert! Stay indoors if possible, wear lightweight and breathable clothing, drink plenty of water 💦, and avoid strenuous activities 🛑.")
-
-def get_rain(data):
-    forecast_list = data["list"]
-    if "rain" in forecast_list[0]:
-        rain = float(forecast_list[0]["rain"]["3h"])
-        if rain < 0.1:
-            print(f"🌤️ Almost no rain expected ({rain:.1f} mm). You’re good to go!")
-        elif 0.1 <= rain < 1.0:
-            print(f"🌦️ Light rain possible ({rain:.1f} mm). You *might* want to bring a small umbrella — just in case.")
-        elif 1.0 <= rain < 3.0:
-            print(f"🌧️ Moderate rain ahead ({rain:.1f} mm). Don’t forget your umbrella or raincoat!")
-        elif 3.0 <= rain < 10.0:
-            print(f"🌧️🌧️ Heavy rain expected ({rain:.1f} mm)! Waterproof gear recommended.")
-        else:
-            print(f"⛈️ Torrential rain alert ({rain:.1f} mm)! Stay safe and dry — avoid unnecessary trips outdoors.")
-    else:
-        print("☀️ No rain in the forecast — enjoy the sunshine!")
-
-def get_humidity(data):
-    forecast_list = data["list"]
-    humidity = forecast_list[0]["main"]["humidity"]
-    if humidity < 30:
-        print(f"🌵 Very dry air today ({humidity}%) — stay hydrated, use lip balm, and maybe a humidifier indoors.")
-    elif 30 <= humidity < 50:
-        print(f"🌤️ Comfortable humidity at ({humidity}%) — enjoy the fresh air!")
-    elif 50 <= humidity < 70:
-        print(f"🌫️ Slightly humid today ({humidity}%) — you might feel a little sticky, so dress in breathable clothes.")
-    elif 70 <= humidity < 85:
-        print(f"💦 It's humid today ({humidity}%) — feels warmer than it is. Stay cool, wear light clothes, and drink plenty of water.")
-    else:  # 85% and above
-        print(f"🥵 Very high humidity! ({humidity}%) — the air is thick and heavy. Stay indoors if possible, avoid intense activity, and keep hydrated.")
-
-def suggestions(url):
-    data = fetch_data(url)
     with open("weather_data.json", "w") as file:
         json.dump(data, file, indent=4)
 
-    get_temp(data)
-    get_rain(data)
-    get_humidity(data)
+
+def store_data():
+    with open("weather_data.json", "r") as file:
+        data = json.load(file)
+
+    database.create_table()
+    database.delete_weather_data()
+
+    for entry in data["list"]:
+        temperature = entry["main"]["temp"]
+        feelslike = entry["main"]["feels_like"]
+        rain = 0
+        if "rain" in entry:
+            rain = entry["rain"]["3h"]
+        humidity = entry["main"]["humidity"]
+        condition = entry["weather"][0]["main"]
+        datetime = entry["dt_txt"]
+        database.insert_weather_data(temperature, feelslike, rain, humidity, condition, datetime)
+
+def get_data():
+    rows = database.show_weather_data()
+    return rows
+
+def give_suggestions():
+    row = get_data()
+    temp = row[1]
+    feels = row[2]
+    rain = row[3]
+    humidity = row[4]
+    condition = str(row[5])
+
+    message = ""
+    if temp < 0:
+        message += f"\n❄️ It’s freezing outside! Temp: {temp}°C, feels like {feels}°C. Wear a heavy winter coat, gloves, and a hat 🧥🧤🧣."
+    elif 0 <= temp < 10:
+        message += f"\n🧥 Quite chilly. Temp: {temp}°C, feels like {feels}°C. A warm jacket and layers are recommended 🧣🧦."
+    elif 10 <= temp < 20:
+        message += f"\n🌤️ Mild weather. Temp: {temp}°C, feels like {feels}°C. A light jacket or sweater should be enough 🧥."
+    elif 20 <= temp < 30:
+        message += f"\n😊 Comfortable and warm. Temp: {temp}°C, feels like {feels}°C. Short sleeves or a T-shirt are perfect 👕."
+    elif 30 <= temp < 40:
+        message += f"\n☀️ It’s hot outside! Temp: {temp}°C, feels like {feels}°C. Wear breathable clothes, stay hydrated 💧🧢."
+    else:
+        message += f"\n🔥 Extreme heat alert! Temp: {temp}°C, feels like {feels}°C. Stay indoors if possible, wear lightweight clothes, drink plenty of water 💦, and avoid strenuous activities 🛑."
+
+    if rain > 0:
+        if rain < 0.1:
+            message += f"\n🌤️ Almost no rain expected ({rain:.1f} mm). You’re good to go!"
+        elif 0.1 <= rain < 1.0:
+            message += f"\n🌦️ Light rain possible ({rain:.1f} mm). Bring a small umbrella — just in case."
+        elif 1.0 <= rain < 3.0:
+            message += f"\n🌧️ Moderate rain ahead ({rain:.1f} mm). Don’t forget your umbrella or raincoat!"
+        elif 3.0 <= rain < 10.0:
+            message += f"\n🌧️🌧️ Heavy rain expected ({rain:.1f} mm)! Waterproof gear recommended."
+        else:
+            message += f"\n⛈️ Torrential rain alert ({rain:.1f} mm)! Stay safe and dry — avoid unnecessary trips outdoors."
+    else:
+        message += "\n☀️ No rain in the forecast — enjoy the sunshine!"
+
+    if humidity < 30:
+        message += f"\n🌵 Very dry air today ({humidity}%) — stay hydrated, use lip balm, and maybe a humidifier indoors."
+    elif 30 <= humidity < 50:
+        message += f"\n🌤️ Comfortable humidity at ({humidity}%) — enjoy the fresh air!"
+    elif 50 <= humidity < 70:
+        message += f"\n🌫️ Slightly humid today ({humidity}%) — you might feel a little sticky, so dress in breathable clothes."
+    elif 70 <= humidity < 85:
+        message += f"\n💦 It's humid today ({humidity}%) — feels warmer than it is. Stay cool, wear light clothes, and drink plenty of water."
+    else:
+        message += f"\n🥵 Very high humidity! ({humidity}%) — the air is thick and heavy. Stay indoors if possible, avoid intense activity, and keep hydrated."
 
 
-schedule.every(3).hours.do(suggestions, url)
+    if condition in ["Clear", "Sunny"]:
+        message += "\n☀️ Clear skies today — perfect for outdoor activities!"
+    elif condition in ["Clouds", "Cloudy", "Overcast"]:
+        message += "\n☁️ Cloudy skies — might be a bit gloomy but still fine for outdoor activities."
+    elif condition in ["Rain", "Drizzle"]:
+        message += "\n🌧️ Rainy weather — bring an umbrella or wear a raincoat!"
+    elif condition in ["Thunderstorm"]:
+        message += "\n⛈️ Thunderstorm alert — stay indoors if possible and stay safe!"
+    elif condition in ["Snow"]:
+        message += "\n❄️ Snowy conditions — dress warmly and be careful on the roads."
+    else:
+        message += f"\n🌤️ Current condition: {condition} — dress appropriately."
+
+
+    wear_advice = "\n\n👕 Recommendation: "
+    if feels < 0:
+        wear_advice += "Heavy winter coat 🧥, layers 🧣, gloves 🧤, hat 🧢"
+    elif 0 <= feels < 10:
+        wear_advice += "Warm jacket 🧥, sweater layers 🧣, scarf 🧤"
+    elif 10 <= feels < 20:
+        wear_advice += "Light jacket 🧥 or long-sleeve shirt 👕"
+    elif 20 <= feels < 30:
+        wear_advice += "T-shirt 👕, breathable top 👚"
+    else:
+        wear_advice += "Light, airy top 👕, stay hydrated 💧"
+
+    if feels < 0:
+        wear_advice += "; thick pants ❄️, thermal leggings 🩳"
+    elif 0 <= feels < 10:
+        wear_advice += "; warm pants 👖, maybe jeans 🩳"
+    elif 10 <= feels < 20:
+        wear_advice += "; pants 👖 or skirt 👗 with tights"
+    elif 20 <= feels < 30:
+        wear_advice += "; light pants 👖, shorts 🩳 optional"
+    else:
+        wear_advice += "; shorts 🩳, skirt 👗, or breathable trousers 👖"
+
+    if rain >= 0.1:
+        wear_advice += ", and take an umbrella ☂️ or raincoat 🌂"
+
+    if humidity >= 70 and feels >= 20:
+        wear_advice += " — light fabrics recommended due to humidity 💦"
+
+    if condition in ["Snow"]:
+        wear_advice += ", plus waterproof boots 🥾 and warm layers ❄️"
+
+    if condition in ["Clear", "Sunny"] and feels >= 30:
+        wear_advice += ", plus sunscreen 🧴, hat 🧢, and sunglasses 🕶️"
+
+    message += wear_advice
+    return message
+
+def send_notification_on_desktop():
+    notification.notify(
+        title="🌤️ Weather & Outfit Advice",
+        message=give_suggestions()
+    )
+
+fetch_data()
 while True:
-    schedule.run_pending()
-    time.sleep(1)
-
-#not finished yet
+    send_notification_on_desktop()
+    time.sleep(3 * 60 * 60)
